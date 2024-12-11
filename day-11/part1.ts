7ef512052fe1984c0082aa79bc86ec8d393ed4e6
Floris Bos <57983252+BluntKatana@users.noreply.github.com>
2024-12-11 11:09:10 +0100

    feat: day11 part2 :)

--- day-11/part1.ts
+++ day-11/part1.ts
@@ -1,41 +1,30 @@
 import { read } from "../utils";
-
-// read from file
+const BLINKS = 25;
 const data = read(11, "input");
 
-let stones: string[] = data.split(" ");
+let stones: number[] = data.split(" ").map(Number);
 
 function blink() {
   const newstones = [];
   for (const stone of stones) {
-    if (stone === "0") {
-      newstones.push("1");
-    } else if (stone.length % 2 === 0) {
-      const splitstones = stone.split("");
-      const pivot = Math.floor(stone.length / 2);
+    if (stone === 0) {
+      newstones.push(1);
+    } else if (stone.toString().length % 2 === 0) {
+      const strStone = stone.toString();
+      const pivot = Math.floor(strStone.length / 2);
+      const leftStone = strStone.substring(0, pivot);
+      const rightStone = strStone.substring(pivot);
 
-      let leftstone = splitstones.splice(0, pivot).join("");
-      while (leftstone[0] === "0" && leftstone.length > 1) {
-        leftstone = leftstone.slice(1);
-      }
-      let rightstone = splitstones.join("");
-      while (rightstone[0] === "0" && rightstone.length > 1) {
-        rightstone = rightstone.slice(1);
-      }
-      newstones.push(leftstone);
-      newstones.push(rightstone);
+      newstones.push(Number(leftStone), Number(rightStone));
     } else {
-      newstones.push((Number(stone) * 2024).toString());
+      newstones.push(Number(stone) * 2024);
     }
   }
-
   stones = newstones;
 }
 
-let blinked = 0;
-while (blinked < 25) {
-  // console.log("after:", blinked, "stones:", stones.join(" "));
+for (let hoi = 0; hoi < BLINKS; hoi++) {
   blink();
-  blinked++;
 }
+
 console.log(stones.length);
--- day-11/part2.ts
+++ day-11/part2.ts
@@ -1,40 +1,39 @@
 import { read } from "../utils";
 
-// read from file
-const data = read(11, "example");
+const BLINKS = 75;
 
-const stonesToProcess: { stone: number; blinked: number }[] = data
-  .split(" ")
-  .map((stone) => ({ stone: Number(stone), blinked: 0 }));
+const data = read(11, "input");
 
-let stone = null;
-let stoneCount = 0;
-const lastStones = [];
-while ((stone = stonesToProcess.pop())) {
-  const copyStone = stone;
-  // process stones until they reach some number
-  while (stone.blinked < 25) {
-    const blinked: number = stone.blinked + 1;
-    if (stone.stone === 0) {
-      stone = { stone: 1, blinked };
-    } else if (numDigits(stone.stone) % 2 === 0) {
-      const strStone = stone.stone.toString();
-      const leftStone = strStone.substring(0, Math.floor(strStone.length / 2));
-      const rightStone = strStone.substring(Math.floor(strStone.length / 2));
+let stonesMap = new Map<number, number>();
+for (const stone of data.split(" ").map(Number)) {
+  stonesMap.set(stone, (stonesMap.get(stone) || 0) + 1);
+}
 
-      stone = { stone: Number(leftStone), blinked };
-      stonesToProcess.push({ stone: Number(rightStone), blinked });
+function blink() {
+  const newStonesMap = new Map<number, number>();
+
+  for (const [stone, count] of stonesMap.entries()) {
+    if (stone === 0) {
+      newStonesMap.set(1, (newStonesMap.get(1) || 0) + count);
+    } else if (stone.toString().length % 2 === 0) {
+      const strStone = stone.toString();
+      const pivot = Math.floor(strStone.length / 2);
+      const leftStone = Number(strStone.substring(0, pivot));
+      const rightStone = Number(strStone.substring(pivot));
+
+      newStonesMap.set(leftStone, (newStonesMap.get(leftStone) || 0) + count);
+      newStonesMap.set(rightStone, (newStonesMap.get(rightStone) || 0) + count);
     } else {
-      stone = { stone: stone.stone * 2024, blinked };
+      const largeStone = stone * 2024;
+      newStonesMap.set(largeStone, (newStonesMap.get(largeStone) || 0) + count);
     }
   }
-  lastStones.push(copyStone.stone);
-  if (stone.blinked !== 25) console.log(stone.blinked);
-  stoneCount++;
+  stonesMap = newStonesMap;
 }
 
-console.log(stoneCount); //, ":", lastStones.join(" "));
-
-function numDigits(x: number) {
-  return (Math.log10((x ^ (x >> 31)) - (x >> 31)) | 0) + 1;
+for (let hoi = 0; hoi < BLINKS; hoi++) {
+  blink();
 }
+
+const stoneCount = stonesMap.values().reduce((acc, val) => acc + val);
+console.log(stoneCount);
